936de6d5af410db952a259995d49bccf2a020bf4
Rayhanuc <rayhanctg4@gmail.com>
2022-11-01 04:29:58 +0600

    035 Outputting Widgets Conditionally

--- lib/main.dart
+++ lib/main.dart
@@ -21,11 +21,17 @@ class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
 
   void _answerQuestion() {
+    var questions;
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
 
     print(_questionIndex);
+    if (_questionIndex < questions.length) {
+      print('We have more questions!');
+    } else {
+      print('No more questions!');
+    }
   }
 
   @override
@@ -57,16 +63,20 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Column(
-          children: [
-            // Question(questions[_questionIndex]['questionText']),
-            Question(questions[_questionIndex]['questionText'] as String),
-            ...(questions[_questionIndex]['answers'] as List<String>)
-                .map((answer) {
-              return Answer(_answerQuestion, answer);
-            }).toList()
-          ],
-        ),
+        body: _questionIndex < questions.length
+            ? Column(
+                children: [
+                  // Question(questions[_questionIndex]['questionText']),
+                  Question(questions[_questionIndex]['questionText'] as String),
+                  ...(questions[_questionIndex]['answers'] as List<String>)
+                      .map((answer) {
+                    return Answer(_answerQuestion, answer);
+                  }).toList()
+                ],
+              )
+            : Center(
+                child: Text('You did it!'),
+              ),
       ),
     );
   }
